08552dabb394066fefe3a2ecb18ffda77abee8b6
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-11-09 17:25:38 +0100

    updt venv.sh

--- venv.sh
+++ venv.sh
@@ -1,5 +1,9 @@
 python3 -m venv venv
-
+source venv/bin/activate
 pip install bs4
 pip install selenium
 pip install requests
+pip install scikit-learn
+pip install pandas
+pip install numpy
+pip install matplotlib
