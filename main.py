b5fa01d564f1e0f42ec6cd4d18dcb5326f397355
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-11-09 20:49:28 +0100

    fix main.py : - on demande des slugs et plus des url

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 from scrapping.main import ecrire_data
 from scrapping.scrap_data import scrap_data
 from scrapping.scrap_liste_recettes import get_all_recipe_slugs
-from scrapping.const import SITE, AVIS, EXTENSION
 
 if __name__ == "__main__":
     slugs = get_all_recipe_slugs()
-    urls = [f"{SITE}{AVIS}{slug}{EXTENSION}" for slug in slugs]
-    comments = scrap_data(urls)
+    comments = scrap_data(slugs)
     ecrire_data(comments)
     print(f"Total commentaires récupérés : {len(comments)}")
