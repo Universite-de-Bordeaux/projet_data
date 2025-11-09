9568c0078ff3a6655e4d0fbfc792a6ef21cfad3a
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2025-11-09 21:03:33 +0100

    ajout comptage donnés uniques

--- reader_data.py
+++ reader_data.py
@@ -21,3 +21,39 @@ def read_data(filename="datas.json") -> dict:
     except FileNotFoundError:
         print(f"Le fichier {filename} n'existe pas.")
         return {}
+
+def extracte_data_unique(filename="datas.json"):
+    """
+    Lit les commentaires depuis un fichier JSON et renvoie un dictionnaire.
+    """
+    try:
+        with open(filename, "r", encoding="utf-16") as f:
+            json_data = json.load(f)
+
+        c = 0
+        set_commentaire = set()
+        set_recipe = set()
+        set_author = set()
+        set_date = set()
+        for com in json_data:
+            set_commentaire.add(com["com_text"])
+            set_recipe.add(com["com_recipe_id"])
+            set_author.add(com["com_author"])
+            set_date.add(com["com_date"])
+            c += 1
+        #print(set_commentaire)
+        print(set_recipe)
+        print(set_author)
+        print(set_date)
+        print(f"{c} commentaires extrait depuis {filename}.")
+        print(f"{len(set_commentaire)} commentaires unique.")
+        print(f"{len(set_recipe)} recette unique.")
+        print(f"{len(set_author)} auteur unique.")
+        print(f"{len(set_date)} dates unique.")
+        return c, len(set_commentaire), len(set_recipe), len(set_author), len(set_date)
+
+    except FileNotFoundError:
+        print(f"Le fichier {filename} n'existe pas.")
+        return {}
+
+extracte_data_unique()
