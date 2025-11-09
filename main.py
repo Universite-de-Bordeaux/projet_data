79a1e579f6df3039b67b9c66515a0162433943dd
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-11-09 20:40:36 +0100

    fix main.py : - changement de la bdd

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """ main.py """
-from scrapping.main import ecrire_comments_dans_fichier
-from scrapping.scrap_com import scrap_comments
+from scrapping.main import ecrire_data
+from scrapping.scrap_data import scrap_data
 from scrapping.scrap_liste_recettes import get_all_recipe_slugs
 from scrapping.const import SITE, AVIS, EXTENSION
 
 if __name__ == "__main__":
     slugs = get_all_recipe_slugs()
     urls = [f"{SITE}{AVIS}{slug}{EXTENSION}" for slug in slugs]
-    comments = scrap_comments(urls)
-    ecrire_comments_dans_fichier(comments)
+    comments = scrap_data(urls)
+    ecrire_data(comments)
     print(f"Total commentaires récupérés : {len(comments)}")
