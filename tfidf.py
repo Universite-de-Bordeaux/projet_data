db328a274bfc3cdc7ddea6826d61535b878eb62b
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2025-11-10 11:40:39 +0100

    ajout chagement matrice ti-idf

--- tfidf.py
+++ tfidf.py
@@ -10,10 +10,13 @@ def calculer_tfidf(corpus: list[str]):
     return tfidf, X_tfidf
 
 def sauvegarder(X_tfidf, filename = "tfidf.json"):
-    json_data = []
+    json_data = [{
+                    "longueur": X_tfidf.shape[0],
+                    "largeur": X_tfidf.shape[1],
+                }]
 
-    for i in range(len(X_tfidf.toarray())):
-        for j in range(len(X_tfidf.toarray()[i])):
+    for i in range(X_tfidf.shape[0]):
+        for j in range(X_tfidf.shape[1]):
             if X_tfidf.toarray()[i][j] != 0:
                 json_data.append({
                     "x": i,
@@ -23,4 +26,17 @@ def sauvegarder(X_tfidf, filename = "tfidf.json"):
 
     with open(filename, "w", encoding="utf-16") as f:
         json.dump(json_data, f, indent=4, ensure_ascii=False)
+
+def load_matrice(filename = "tfidf.json"):
+    with open(filename, "r", encoding="utf-16") as f:
+        json_data = json.load(f)
+
+    size = json_data.pop(0)
+    matrix = []
+    for i in range(size["longueur"]):
+        l = [0] * size["largeur"]
+        matrix.append(l)
+    for data in json_data:
+        matrix[data["x"]][data["y"]] = data["valeur"]
+    return matrix
 
