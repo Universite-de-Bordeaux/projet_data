7c0df1d35a8ef88d12fc8b3c3c0fa374434368e0
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-11-10 10:43:56 +0100

    updt cartographie.py : - prend un filename optionel en entrée - ne dépend plus du dataset

--- cartographie.py
+++ cartographie.py
@@ -4,16 +4,8 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import plotly.express as px
 
-def projec_pca_interactive(datas, X_tfidf) -> None:
+def projec_pca_interactive(datas, X_tfidf, filename="projection_pca_interactive.html") -> None:
     """ Crée une projection PCA interactive des commentaires agrégés par recette. """
-
-    # Reconstruction des textes par recette dans le même ordre que X_tfidf
-    groupes = {}
-    for _, (text, recipe_id, _, _) in datas.items():
-        groupes.setdefault(recipe_id, []).append(text)
-
-    # Pour hover, on concatène tous les commentaires d'une recette
-    comments_by_recipe = [";".join(texts) for recipe_id, texts in groupes.items()]
 
     # Calcul de la PCA
     pca = PCA(n_components=2)
@@ -23,7 +15,7 @@ def projec_pca_interactive(datas, X_tfidf) -> None:
     df = pd.DataFrame({
         'x': X_pca[:,0],
         'y': X_pca[:,1],
-        'comment': comments_by_recipe
+        'comment': datas
     })
 
     # Scatter interactif
@@ -35,5 +27,5 @@ def projec_pca_interactive(datas, X_tfidf) -> None:
     )
 
     # Sauvegarde interactive en HTML
-    fig.write_html("projection_pca_interactive.html")
-    print("Carte interactive sauvegardée sous 'projection_pca_interactive.html'")
+    fig.write_html(filename)
+    print(f"Carte interactive sauvegardée sous {filename}")
